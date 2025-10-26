9fe5fb4ff0f077fd44f1e7cbefccb1430964856b
k-dx <tornadowro@gmail.com>
2025-10-26 16:02:06 +0100

    client2: counter

--- client2/app/build.gradle.kts
+++ client2/app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation("androidx.preference:preference-ktx:1.2.1")
 }
--- client2/app/src/main/java/xyz/jdubiel/migawka/MainActivity.kt
+++ client2/app/src/main/java/xyz/jdubiel/migawka/MainActivity.kt
@@ -4,23 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import xyz.jdubiel.migawka.ui.theme.MigawkaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             MigawkaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    Migawka(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +39,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Migawka(name: String, modifier: Modifier = Modifier) {
+    var counter by remember { mutableIntStateOf(0) }
+    Column {
+        Text(
+            text = "Hello $name! x $counter",
+            modifier = modifier
+        )
+        Button(onClick = { counter++ }) {
+            Text(
+                text = stringResource(R.string.settings)
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MigawkaPreview() {
     MigawkaTheme {
-        Greeting("Android")
+        Migawka("Android")
     }
 }
