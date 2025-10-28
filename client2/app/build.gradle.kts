02392a4e9d7fe3e128ed8b7748bf65ac3c7fb145
k-dx <tornadowro@gmail.com>
2025-10-28 15:19:53 +0100

    client2: cleanup

--- client2/app/build.gradle.kts
+++ client2/app/build.gradle.kts
@@ -57,6 +57,4 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-
-    implementation("androidx.preference:preference-ktx:1.2.1")
 }
