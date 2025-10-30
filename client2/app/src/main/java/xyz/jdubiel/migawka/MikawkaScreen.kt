4a2bcf92ef455c2ee4bd609249331f58c720c346
k-dx <tornadowro@gmail.com>
2025-10-30 11:46:11 +0100

    client2: remove unused import

--- client2/app/src/main/java/xyz/jdubiel/migawka/MikawkaScreen.kt
+++ client2/app/src/main/java/xyz/jdubiel/migawka/MikawkaScreen.kt
@@ -1,6 +1,5 @@
 package xyz.jdubiel.migawka
 
-import android.widget.Gallery
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
