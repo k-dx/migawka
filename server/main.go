25806c00a5115fe1b157738ab991ab17ab5a49fc
k-dx <tornadowro@gmail.com>
2025-11-10 16:41:13 +0100

    server: add media store to server struct

--- server/main.go
+++ server/main.go
@@ -22,6 +22,17 @@ var (
 // server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.UnimplementedGreeterServer // TODO: rename to MigawkaServer
+	mediaStore                    MediaStore
+}
+
+func CreateServer(mediaDirectory string) *server {
+	mediaStore, err := NewMediaStore(mediaDirectory)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create media store")
+	}
+	return &server{
+		mediaStore: mediaStore,
+	}
 }
 
 // SayHello implements helloworld.GreeterServer
@@ -74,7 +85,18 @@ func initLogger(logLevel *string) {
 
 func main() {
 	logLevel := flag.String("loglevel", "warn", "Log level: debug, info, warn, error, fatal, panic")
+	MEDIA_DIR_ARG := "mediadir"
+	mediaDirectory := flag.String(MEDIA_DIR_ARG, "", "Path to media directory (required)")
+
 	flag.Parse()
+
+	if *mediaDirectory == "" {
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "	-%s is required!\n", MEDIA_DIR_ARG)
+		fmt.Fprintf(os.Stderr, "\n")
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	initLogger(logLevel)
 
@@ -82,10 +104,13 @@ func main() {
 	if err != nil {
 		log.Fatal().Msgf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+
+	grpcServer := grpc.NewServer()
+	migawkaServer := CreateServer(*mediaDirectory)
+
+	pb.RegisterGreeterServer(grpcServer, migawkaServer)
 	log.Info().Msgf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
+	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatal().Msgf("failed to serve: %v", err)
 	}
 }
